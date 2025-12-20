20b6d317672af2c1d28c15ae299a94665de7e1f8
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2025-12-20 08:29:42 +0000

    commit

--- backend/index.js
+++ backend/index.js
@@ -4,26 +4,16 @@ import cookieParser from 'cookie-parser'
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js'
 import cors from 'cors'
-import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin: "https://backend-bj7u.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://frontend-bshr.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-const __dirname = path.resolve();
 
 app.use('/api/auth',authRoutes)
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT,(req,res)=>{
     connectDB();
